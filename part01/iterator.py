0e6893a5543fdac36ea86420cbc10cbde172b4bb
Hisao Soyama <hisao.soyama@gmail.com>
2015-01-04 16:18:02 +0900

    Rename part1 -> part01

--- part01/iterator.py
+++ part01/iterator.py
@@ -30,7 +30,7 @@ class BookShelf(Aggregate):
     def get_book_at(self, index):
         return self.__books[index]
 
-    def append_book(self,book):
+    def append_book(self, book):
         self.__books.append(book)
 
     def get_length(self):
